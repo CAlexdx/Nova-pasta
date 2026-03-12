6c828a23eca97b614a580e39b51a79d01b9efc99
CAlexdx <00001094308602sp@aluno.educacao.sp.gov.br>
2026-03-12 10:06:28 -0300

    Readme e controller

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const pessoaRoutes = require('./routes/pessoa'); // Importando as rotas
+const pessoaRoutes = require('./routes/pessoa');
 
 const app = express();
 app.use(express.json());
 
-// Usando as rotas importadas
+// Rota para a raiz (Evita o erro "Cannot GET /")
+app.get('/', (req, res) => {
+    res.send(`
+        <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+            <h1>🚀 API Node.js + SQLite está ON!</h1>
+            <p>Use o endpoint <a href="/pessoas">/pessoas</a> para acessar os dados.</p>
+            <p>Desenvolvido por Calebe Alves Câmara</p>
+        </div>
+    `);
+});
+
+// Registrar as rotas do módulo de pessoas
 app.use(pessoaRoutes);
 
 const PORT = 3000;
