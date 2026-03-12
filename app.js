5750bb5ed8e573200ca663704129fe018aea597d
CAlexdx <00001094308602sp@aluno.educacao.sp.gov.br>
2026-03-12 10:16:02 -0300

    Readme e controller

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const cors = require('cors'); // Novo pacote
 const pessoaRoutes = require('./routes/pessoa');
 
 const app = express();
-app.use(express.json());
 
-// Rota para a raiz (Evita o erro "Cannot GET /")
+// --- MIDDLEWARES ---
+
+app.use(express.json());
+app.use(cors()); // Libera o acesso para qualquer origem
+
+// Middleware de Log (Exemplo do que o professor ensina sobre fluxo)
+app.use((req, res, next) => {
+    const data = new Date().toLocaleString();
+    console.log(`[${data}] ${req.method} na rota ${req.url}`);
+    next(); // Passa para a próxima função (essencial!)
+});
+
+// --- ROTAS ---
+
 app.get('/', (req, res) => {
     res.send(`
         <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
             <h1>🚀 API Node.js + SQLite está ON!</h1>
             <p>Use o endpoint <a href="/pessoas">/pessoas</a> para acessar os dados.</p>
-            <p>Desenvolvido por Calebe Alves Câmara</p>
+            <p>Status: <strong>Pronto para conexões externas (CORS Ativado)</strong></p>
         </div>
     `);
 });
 
-// Registrar as rotas do módulo de pessoas
 app.use(pessoaRoutes);
 
 const PORT = 3000;
